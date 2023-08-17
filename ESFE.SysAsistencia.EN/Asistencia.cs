7a5135025e044aa7e41866a8259f30e3643f0fc9
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-16 21:00:45 -0600

    chore: :sparkle: add class asistencia

---
+++ ESFE.SysAsistencia.EN/Asistencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfe.SysAsistencia.EN
+{
+    /// <summary>
+    /// Clase que representa a un Estudiante.
+    /// </summary>
+    public class Asistencia
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int EstudianteId { get; set; }
+        public int CriterioId { get; set; }
+        public int NomenclaturaId { get; set; }
+    }
+}
---
+++ ESFE.SysAsistencia.EN/Huella.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfe.SysAsistencia.EN
+{
+    /// <summary>
+    /// Clase que representa a un Estudiante.
+    /// </summary>
+    public class Huella
+    {
+        public int Id { get; set; }
+        public byte[] Muestra { get; set; }
+        public int RolId { get; set; }
+        public int DocenteId { get; set; }
+        public int EstudianteId { get; set; }
+    }
+}
