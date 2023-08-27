40d204203504a143caa1818350ee138e76a6215b
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-27 14:38:52 -0600

    chore: :sparkles: rfid methods

---
+++ ESFE.SysAsistencia.BL/RfidBL.cs
@@ -0,0 +1,19 @@
+﻿using ESFE.SysAsistencia.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.SysAsistencia.BL
+{
+    public class RfidBL
+    {
+        RfidDAL rfidDAL = new RfidDAL();
+
+        public async Task<bool> PostUid(string uid, int estudianteId)
+        {
+           return await rfidDAL.PostUid(uid, estudianteId);
+        }
+    }
+}
---
+++ ESFE.SysAsistencia.DAL/RfidDAL.cs
@@ -0,0 +1,51 @@
+﻿using ESFE.SysAsistencia.EN;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static ESFE.SysAsistencia.DAL.ComunApi;
+
+namespace ESFE.SysAsistencia.DAL
+{
+    public  class RfidDAL
+    {
+        public async Task<bool> PostUid(string uid, int estudianteId)
+        {
+            var request = new RestRequest("/rfid", Method.Post);
+            var cancellationToken = new CancellationToken(); // Debes definir un token de cancelación
+
+            // Agregar los parámetros en el cuerpo de la solicitud
+            request.AddBody(new { uid, estudianteId});
+
+            try
+            {
+                var response = await restClient.ExecuteAsync(request, cancellationToken);
+
+                // Procesar la respuesta aquí
+                if (response != null) // Verifica si la respuesta fue exitosa (código 200)
+                {
+
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Error en la solicitud:");
+                    if (response != null)
+                    {
+                        Console.WriteLine("Código de estado HTTP: " + response.StatusCode);
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excepción al realizar la solicitud:");
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
